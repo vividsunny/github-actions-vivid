55d88be5f10289858380b1f70c7c21dbf323fad7
vividsunny <vivid.sunnypatel@gmail.com>
2022-02-10 11:44:56 +0530

    Regular auto-commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h3>Bingo 😜 Bingo</h3>
+        <h3>Bingo 😜 Vivid </h3>
       </header>
     </div>
   );
